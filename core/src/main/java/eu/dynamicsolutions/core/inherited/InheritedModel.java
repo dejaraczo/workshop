81346107a21093d743c1100ef710e820d6acc00d
Krzysztof Jaroński <krzysztof.jaronski@intive.com>
2018-11-22 09:26:50 +0100

    It made my day !

---
+++ core/src/main/java/eu/dynamicsolutions/core/inherited/InheritedModel.java
@@ -0,0 +1,44 @@
+package eu.dynamicsolutions.core.inherited;
+
+import com.day.cq.wcm.api.Page;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+@Model(adaptables = SlingHttpServletRequest.class)
+public class InheritedModel {
+
+    @Inject
+    private Page currentPage;
+
+    @Inject
+    private String nodeName;
+
+    private String path;
+
+    @PostConstruct
+    protected void init() {
+        findNodePath();
+    }
+
+    private void findNodePath() {
+        Page page = currentPage;
+
+        while (page.getContentResource().getChild(nodeName) == null && page.getParent() != null) {
+            page = page.getParent();
+        }
+
+        Resource content = page.getContentResource().getChild(nodeName);
+        if (content != null) {
+            path = content.getPath();
+        }
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
